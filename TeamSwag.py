4ced01132616c666f731d3eeae7acc7770600702
Adrien Katsuya Tateno <adrien.k.tateno@gmail.com>
2014-06-09 05:17:54 -0500

    Made basic AI player

--- TeamSwag.py
+++ TeamSwag.py
@@ -1,6 +1,8 @@
-# Human Player
+from pprint import pprint
 
-class HumanB:
+# Team Swag
+
+class TeamSwag:
 
 	def __init__(self):
 		self.board = [[' '] * 8 for i in range(8)]
@@ -12,11 +14,14 @@ class HumanB:
 		# a list of unit vectors (row, col)
 		self.directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
 
-	def get(self, coord):
-		return self.board[coord[0]][coord[1]]
+	def copy(self, board):
+		return [list(row) for row in board]
 
-	def set(self, coord, value):
-		self.board[coord[0]][coord[1]] = value
+	def get(self, coord, board):
+		return board[coord[0]][coord[1]]
+
+	def set(self, coord, value, board):
+		board[coord[0]][coord[1]] = value
 
 	def add(self, v1, v2):
 		return (v1[0] + v2[0], v1[1] + v2[1])
@@ -30,8 +35,8 @@ class HumanB:
 	def on_board(self, v):
 		return v[0] >= 0 and v[0] < self.size and v[1] >= 0 and v[1] < self.size
 
-	def legal(self, coord, mine, their):
-		if self.get(coord) != ' ':
+	def legal(self, coord, mine, their, board):
+		if self.get(coord, board) != ' ':
 			return False
 
 		for v in self.directions:
@@ -40,47 +45,52 @@ class HumanB:
 			pos = next
 			next = self.add(pos, v)
 			while self.on_board(next):
-				if self.get(pos) != their:
+				if self.get(pos, board) != their:
 					break
-				if self.get(next) == mine and self.get(pos) == their:
+				if self.get(next, board) == mine and self.get(pos, board) == their:
 					return True
 				pos = next
 				next = self.add(pos, v)
 		return False
 
-	def flip(self, coord, v, distance, mine):
+	def flip(self, coord, v, distance, mine, board):
 		for i in range(distance):
-			self.set(self.ray(coord, v, i + 1), mine)
+			self.set(self.ray(coord, v, i + 1), mine, board)
 
 	def place_piece(self, row, col, mine, their):
-		self._place_piece((row, col), mine, their)
+		self._place_piece((row, col), mine, their, self.board)
 
-	def _place_piece(self, coord, mine, their):
-		self.set(coord, mine)
+	def _place_piece(self, coord, mine, their, board):
+		self.set(coord, mine, board)
 		for v in self.directions:
 			pos = coord
 			pos = self.add(pos, v)
 			distance = 0
 			while self.on_board(pos):
-				if self.get(pos) == ' ':
+				if self.get(pos, board) == ' ':
 					break
-				if self.get(pos) == mine:
-					self.flip(coord, v, distance, mine)
+				if self.get(pos, board) == mine:
+					self.flip(coord, v, distance, mine, board)
 					break
 				distance = distance + 1
 				pos = self.add(pos, v)
 
+	# Evaluation function
+
+	def evaluate(self, mine, their, board):
+		pass
+
+	def minimax(self, curr, mine, their, board):
+		for i in range(self.size):
+			for j in range(self.size):
+				if self.legal((i, j), mine, their, board):
+					return (i, j)
 
 	def play_square(self, prev_row, prev_col, mine, their):
 		prev = (prev_row, prev_col)
 		if prev != (-1, -1):
-			self._place_piece(prev, their, mine)
-		coord = (-1, -1)
-		while not self.legal(coord, mine, their):
-			try:
-				coord = (int(raw_input('Row: ')) - 1, int(raw_input('Col: ')) - 1)
-			except KeyboardInterrupt:
-				raise SystemExit(0)
-			except ValueError:
-				pass
-		return coord
+			self._place_piece(prev, their, mine, self.board)
+		pprint(self.board)
+
+		move = self.minimax(mine, mine, their, self.copy(self.board))
+		return move
